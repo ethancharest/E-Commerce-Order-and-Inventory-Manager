fbf7d1b043072fa931df53497534bb4d11396580
ethan charest <ethancharest1@gmail.com>
2025-10-06 15:34:23 -0400

    Added some user code.

--- ecommerce/model/User.java
+++ ecommerce/model/User.java
@@ -1,5 +1,18 @@
 package ecommerce.model;
 
 public class User {
-
+    private final String id;
+    private String name;
+    private Role role;
+    private String username;
+    private String password;
+    private User(String id, String name, Role role, String username, String password) {
+        this.id = id;
+        this.name = name;
+        this.role = role;
+        this.username = username;
+        this.password = password;
+    }
+    @Override public String toString() { return name + " (" + role + ")";
+    }
 }
